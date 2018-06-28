3f6e9342348d3fa2db2127c18726a017382d13a9
TJ Balon <balon@tangoworldwide.net>
2018-06-28 14:20:09 -0400

    File name changes, removal of functioins, merge ECC into crypto-tools

--- ECC/ecaddition.py
+++ ECC/ecaddition.py
@@ -1,55 +1,57 @@
 # ecaddition.py
-# Implementation of El Gamal using DLP crypto
+# Elliptic Curve Addition Function
 # 
 # Authored by TJ Balon (@tjbalon)
-
 import cryptotools as crypto
 
+# Global Variables
 EI = 3.14
+x = 0
+y = 1
 
-def ecaddition(prime, A, p, q):
-	result = check1(p, q)
+def add(prime, eqvarA, pointP, pointQ):
+	result = infinityCheck(pointP, pointQ)
 	if result != 0:
 		return result
 
-	result = check2(p, q, prime)
+	result = inverseCheck(pointP, pointQ, prime)
 	if result != 0:
 		return result
 	
-	return other(prime, A, p, q)
+	return ecaddition(prime, eqvarA, pointP, pointQ)
 
-def check1(p, q):	
-	if p == EI:
-		return q
-	elif q == EI:
-		return p
+def infinityCheck(pointP, pointQ):	
+	if pointP == EI:
+		return pointQ
+	elif pointQ == EI:
+		return pointP
 	else:
 		return 0
 
-def check2(p, q, prime):
-	if p[0] == q[0] and p[1] == (-q[1] % prime):
+def inverseCheck(pointP, pointQ, prime):
+	if pointP[x] == pointQ[x] and pointP[y] == (-pointQ[y] % prime):
 		return EI
 	else:
 		return 0
 
-def other(prime, A, p, q):
-	if(p == q):
-		response = ((3 * pow(p[0], 2) + A) * crypto.inverse(q[0] - p[0]) % prime)
+def ecaddition(prime, eqvarA, pointP, pointQ):
+	if(pointP == pointQ):
+		eqLambda = ((3 * pow(pointP[x], 2) + eqvarA) * (crypto.inverse(2 * pointP[y], prime)) % prime)
 	else:
-		response = (((q[1] - p[1]) * crypto.inverse(q[0] - p[0], prime)) % prime)
+		eqLambda = (((pointQ[y] - pointP[y]) * crypto.inverse(pointQ[x] - pointP[x], prime)) % prime)
 
-	x3 = ((pow(response, 2) - p[0] - q[0]) % prime)
-	y3 = ((((response * p[0]) - (response * x3)) - p[1]) % prime)
+	x3 = ((pow(eqLambda, 2) - pointP[x] - pointQ[x]) % prime)
+	y3 = ((((eqLambda * pointP[x]) - (eqLambda * x3)) - pointP[y]) % prime)
 
 	coords = (x3, y3)
 	return coords
 
 def main():
 	prime = 13
-	bigA = 3
-	p = (9, 6)
-	q = (9, 7)
-	print(ecaddition(prime, bigA, p, q))
+	eqvarA = 3
+	pointP = (1, 8)
+	pointQ = (1, 8)
+	print(add(prime, eqvarA, pointP, pointQ))
 
 
 if __name__ == "__main__":
